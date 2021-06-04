9198f2ed82d373f3f5495ec9c70070bff3a9d856
Ivan <ivanrj7j@gmail.com>
2021-06-04 11:17:25 +0530

    commented out the sign in javascript

--- static/signin.js
+++ static/signin.js
@@ -1,22 +1,29 @@
 var signin_btn = document.querySelector('#submit');
 var email = document.querySelector('#email');
 var password = document.querySelector('#pass');
+// hooking up the buttons and inputs 
 
 
 signin_btn.addEventListener('click', () => {
     var email_value = email.value;
     var pass = sha256(password.value);
+    // getting the email and the password(encoded)
 
     var xhr = new XMLHttpRequest();
     var form = new FormData();
+    // creating objects of formdata and xhr 
 
     form.append('email', email_value);
     form.append('pass', pass);
+    // appending the value of email and password to the formdata 
 
     xhr.open('POST', '/check_login');
+    // connecting to the server 
     xhr.send(form);
+    // sending the data 
     xhr.addEventListener('load', () => {
         var response = xhr.response;
+        // getting the response 
 
         if (response == "success") {
             location.href = '/';
@@ -27,7 +34,7 @@ signin_btn.addEventListener('click', () => {
                 alert("Check your password again");
                 location.reload();
             }
-
+            // handling the response 
         }
     });
 });
